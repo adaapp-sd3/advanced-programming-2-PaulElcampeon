dc0bbfe631ae073f06d00b3f00a04ddc456504da
PaulElcampeon <pauloladele@live.co.uk>
2019-05-03 20:57:49 +0100

    dto for weather request

---
+++ src/main/java/com/paul/farm/dtos/WeatherResDto.java
@@ -0,0 +1,12 @@
+package com.paul.farm.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WeatherResDto {
+
+    private String weather;
+
+}
