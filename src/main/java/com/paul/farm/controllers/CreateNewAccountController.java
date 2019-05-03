4273fc3f909a4af32e309d3c7afb77146e1a93d4
PaulElcampeon <pauloladele@live.co.uk>
2019-05-03 20:26:42 +0100

    controller to handle new account creation

---
+++ src/main/java/com/paul/farm/controllers/CreateNewAccountController.java
@@ -0,0 +1,36 @@
+package com.paul.farm.controllers;
+
+import com.paul.farm.dtos.CreateNewAccountReqDto;
+import com.paul.farm.services.interfaces.FarmService;
+import com.paul.farm.services.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+public class CreateNewAccountController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private FarmService farmService;
+
+    @RequestMapping(value = "/create-account", method = RequestMethod.POST)
+    public String createAccount(@ModelAttribute @Valid CreateNewAccountReqDto createNewAccountReqDto, BindingResult result) {
+        System.out.println(createNewAccountReqDto);
+
+        if (result.hasErrors()) {
+            return "/create-account";
+        }
+
+        userService.createNewUser(createNewAccountReqDto);
+        farmService.createFarm(createNewAccountReqDto.getUsername());
+        return "redirect:/login";
+    }
+}
