fb83856760e945581d1949cb6456bc4fd4fbc67f
PaulElcampeon <pauloladele@live.co.uk>
2019-05-03 21:09:00 +0100

    code to deal with websocket connection to server

---
+++ src/main/resources/static/js/connection.js
@@ -0,0 +1,34 @@
+var socket, stompClient;
+
+function connect() {
+
+    socket = new SockJS('/farm');
+
+    stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+
+        stompClient.subscribe('/topic/farm/' + getFarmName(), function (data) {
+            handleIncomingResponse(JSON.parse(data.body));
+        });
+
+        stompClient.subscribe('/topic/farm/message/' + getFarmName(), function (data) {
+            handleErrorMessage(JSON.parse(data.body));
+        });
+
+        stompClient.send("/app/farm/get/" + getFarmName(), {},);
+
+    }, function (error) {
+
+        console.log(error);
+    });
+}
+
+function handleIncomingResponse(data) {
+    renderFarm(data);
+    renderDashboard(data);
+}
+
+function handleErrorMessage(data) {
+    setMessage(data.message);
+}
