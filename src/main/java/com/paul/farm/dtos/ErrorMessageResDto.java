60306dc1f318268fc77ebea77ac920524fae7314
PaulElcampeon <pauloladele@live.co.uk>
2019-05-03 21:03:38 +0100

    dto for error messages to send to client

---
+++ src/main/java/com/paul/farm/dtos/ErrorMessageResDto.java
@@ -0,0 +1,11 @@
+package com.paul.farm.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageResDto {
+
+    private String message;
+}
