f9f50489643cf068f417770194aaf9cb6522f46e
PaulElcampeon <pauloladele@live.co.uk>
2019-05-06 21:20:29 +0100

    handling used username

--- src/main/java/com/paul/farm/controllers/CreateNewAccountController.java
+++ src/main/java/com/paul/farm/controllers/CreateNewAccountController.java
@@ -5,7 +5,11 @@ import com.paul.farm.services.interfaces.FarmService;
 import com.paul.farm.services.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,11 +30,15 @@ public class CreateNewAccountController {
     private Logger loggger = Logger.getLogger(CreateNewAccountController.class.getName());
 
     @RequestMapping(value = "/create-account", method = RequestMethod.POST)
-    public String createAccount(@ModelAttribute @Valid CreateNewAccountReqDto createNewAccountReqDto, BindingResult result) {
+    public String createAccount(@ModelAttribute @Valid CreateNewAccountReqDto createNewAccountReqDto, BindingResult result, Model model) {
+
+        if (userService.checkIfUserExists(createNewAccountReqDto.getUsername())) {
+            return "redirect:/create-account?error";
+        }
 
         if (result.hasErrors() || userService.checkIfUserExists(createNewAccountReqDto.getUsername())) {
             loggger.log(Level.INFO, String.format("%s requested to create a new account but details were rejected", createNewAccountReqDto.getUsername()));
-            return "/create-account";
+            return "create-account";
         }
 
         loggger.log(Level.INFO, String.format("%s requested to created a new account", createNewAccountReqDto.getUsername()));
